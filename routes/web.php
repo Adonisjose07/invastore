af1cd3f5b210d43c4b3232500e99a66c125eb32e
Adonisjose07 <adonisjose07@gmail.com>
2022-06-16 03:31:39 -0400

    Basic login and registration added with laravel/Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Home;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +13,12 @@ use App\Http\Controllers\Home;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', Home::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
